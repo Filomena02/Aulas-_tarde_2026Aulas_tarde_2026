8b87128d373d3899ea92da0c43e5b64817985553
Filomena02 <filokeller399@gmail.com>
2026-03-17 17:00:37 -0300

    subindo aula

---
+++ Aula_16_26/cabecalho.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Aula 16/03</title>
+	
+		<meta name="autor"content="Filomena">
+		<meta name="keywords"content="PHP, variaveis, se, para, vetores">
+		<meta name="descrition"content="Página sobre PHP">
+		<link rel="icon"href="img/logo.png">
+		<link rel="stylesheet"href="css/estilo.css">
+</head>
+<body>
+
---
+++ Aula_16_26/index.php
@@ -0,0 +1,34 @@
+<?php require_once'cabecalho.php';?>
+<section class="topo">
+	<div id="logo">
+		<img src="img/logo.png"alt="logo">
+	</div>
+	<div id="menu">
+		<ul class="nav">
+			<li>Básico
+				<ol>
+					<li><a href="operações.php"
+						target="janela">Operações</a></li>
+					<li><a href="soma.php"
+						target="janela">soma</a></li>
+					<li><a href="produto.php"
+						target="janela">produto</a></li>
+					<li><a href="salário.php"
+						target="janela">salário</a></li>
+					</ol>
+				</li>
+
+			</ul>
+		</div>
+	</section>
+	</section class="principal">
+	<iframe src="" name="janela"></iframe>
+</section>
+</body>
+	</html>
+
+
+						
+						
+
+
